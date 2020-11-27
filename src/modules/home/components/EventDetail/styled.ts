c5081030b0d7b2468985bbf65a2fe10286b0bfd8
thun <nathun.thun@hotmail.com>
2020-11-27 19:17:18 +0700

    init deploy

--- src/modules/home/components/EventDetail/styled.ts
+++ src/modules/home/components/EventDetail/styled.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
-
 import Text from 'common/components/Text'
+
 
 export const DetailText = styled(Text)`
     line-height:2.5;
     @media screen and (max-width: 768px){
-        line-height:2;
+        line-height:1.0;
     }
 `
